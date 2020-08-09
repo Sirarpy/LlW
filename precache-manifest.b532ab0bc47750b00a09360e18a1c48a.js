5b708309490b4dd59b45e709fed1c9836fa07026
Sirarpy <sipadavtyan@gmail.com>
2020-08-09 19:26:43 +0400

    Updates

---
+++ precache-manifest.b532ab0bc47750b00a09360e18a1c48a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39e519704eb9ff32cb84344e123b816e",
+    "url": "/LlW/index.html"
+  },
+  {
+    "revision": "9cc6eb03bf96de9341e4",
+    "url": "/LlW/static/css/main.dbfd3732.chunk.css"
+  },
+  {
+    "revision": "09595cde52b70fde7fca",
+    "url": "/LlW/static/js/2.e4247480.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LlW/static/js/2.e4247480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cc6eb03bf96de9341e4",
+    "url": "/LlW/static/js/main.2d93d163.chunk.js"
+  },
+  {
+    "revision": "9edb7d3073e24ee3da71",
+    "url": "/LlW/static/js/runtime-main.05c23147.js"
+  },
+  {
+    "revision": "85d339d916479f729938d2911b85bf1f",
+    "url": "/LlW/static/media/Lato-Bold.85d339d9.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/LlW/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/LlW/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/LlW/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "f9b1cd9d52b1d6542c6ec8920060b3b2",
+    "url": "/LlW/static/media/about.f9b1cd9d.jpg"
+  },
+  {
+    "revision": "e527e1af8c582d9b27552273e1e31e9c",
+    "url": "/LlW/static/media/bg01.e527e1af.jpg"
+  },
+  {
+    "revision": "48070e9c3137b46d5de4120751d43841",
+    "url": "/LlW/static/media/bg02.48070e9c.jpg"
+  },
+  {
+    "revision": "ba364128d232df518b199943cab0fc6b",
+    "url": "/LlW/static/media/home-img04.ba364128.jpg"
+  },
+  {
+    "revision": "d95fb74fe2fcfc54e362c63f6375493f",
+    "url": "/LlW/static/media/home-img05.d95fb74f.jpg"
+  },
+  {
+    "revision": "56e6d492049b13d03774be4a75bb9892",
+    "url": "/LlW/static/media/home-img06.56e6d492.jpg"
+  },
+  {
+    "revision": "f9312bcb03bf0d30ea49c2fc4fff3537",
+    "url": "/LlW/static/media/logo.f9312bcb.png"
+  }
+]);
